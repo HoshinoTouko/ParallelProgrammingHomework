1d0283f39b11236e2dfc27550271c11d54e24994
Hoshino Touko <oranly@hotmail.com>
2019-04-21 23:37:37 +0800

    Serial summation.

--- programs/1.2.1.cpp
+++ programs/1.2.1.cpp
@@ -1,7 +1,33 @@
 /*
- *
- * 
- * 
+ * Purpose:
+ *      Serial summation.
+ * Compile:
+ *      g++ 1.2.1.cpp
+ * Execute:
+ *      ./a.out
  */
 #include<cstdio>
+#include<ctime>
 #include<iostream>
+
+#define VECTOR_SIZE 1000
+
+// Generate a vector to do sum operation.
+void generate_vector(int vector[]);
+
+void generate_vector(int vector[]){
+    for(int i = 0; i < VECTOR_SIZE; i++){
+        vector[i] = 10;
+    }
+}
+
+int main(){
+    int sum = 0;
+    int vector[VECTOR_SIZE];
+    generate_vector(vector);
+    for(int i = 0; i < VECTOR_SIZE; i++){
+        sum += vector[i];
+    }
+    std::cout<<sum<<std::endl;
+    return 0;
+}
